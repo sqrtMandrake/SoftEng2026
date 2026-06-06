8168a563ca4a30845482c526b2b1741bf9979711
coolkettle <SwiniawKoronie6@o2.pl>
2026-06-06 15:36:08 +0200

    add test 2 code
    Test: Alan, Code: Passed immediately

--- Project/speed-split/test_scene.gd
+++ Project/speed-split/test_scene.gd
@@ -1,11 +1,29 @@
 extends Node2D
 
-@onready var main: Node2D = $Main
+const MAIN = preload("uid://b6q4d3vsu6ry8")
+
+var timer: Node2D
 
 func _ready() -> void:
 	test_timer_start()
+	test_timer_precision()
 
-func test_timer_start():
-	main.start_timer()
+func create_new_timer() -> void:
+	timer = MAIN.instantiate()
+	add_child(timer)
+
+func test_timer_start() -> void:
+	create_new_timer()
+	timer.start_timer()
 	await get_tree().process_frame
-	assert(main.get_ticks() > 0)
+	assert(timer.get_ticks() > 0, "Timer didn't start!")
+
+func test_timer_precision() -> void:
+	create_new_timer()
+	var start_timestamp: int = Time.get_ticks_usec()
+	timer.start_timer()
+	
+	while Time.get_ticks_usec() - start_timestamp < 1000000:
+		pass
+	print(timer.get_ticks())
+	assert(abs(timer.get_ticks() - 1000000) < 1000, "Timer is more than 1ms imprecise!")
